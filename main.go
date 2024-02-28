67a1dae6c7fa27d6acbce8516d7543a211345c6e
Alona Paz <alkaplan@redhat.com>
2024-02-28 21:44:05 +0200

    operator-sdk create api --group self.service --version v1 --kind OverlayNetwork --controller Create Resource [y/n] y
    Signed-off-by: Alona Paz <alkaplan@redhat.com>

--- main.go
+++ main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	selfservicev1 "github.com/AlonaKaplan/selfserviceoverlay/api/v1"
+	"github.com/AlonaKaplan/selfserviceoverlay/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(selfservicev1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -85,6 +89,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.OverlayNetworkReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "OverlayNetwork")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
